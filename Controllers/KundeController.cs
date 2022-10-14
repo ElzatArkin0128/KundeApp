4bc66a3b9911246de0e9ae02806faddada5c48b7
elzatarkin <s354390@oslomet.no>
2022-10-14 12:04:32 +0200

    testing

---
+++ Controllers/KundeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using KundeApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KundeApp.Controllers
+{
+    [Route("[controller]/[action]")]
+    public class KundeController :ControllerBase
+    {
+        public List<Kunde> HentAlle()
+        {
+            var kundene = new List<Kunde>();
+
+            var kunde1 = new Kunde();
+            kunde1.navn = "Per Hansen";
+            kunde1.adresse = "Osloveien 2";
+
+            var kunde2 = new Kunde()
+            {
+                navn = "Line Hansen",
+                adresse = "Drammenveien 2"
+            };
+
+            kundene.Add(kunde1);
+            kundene.Add(kunde2);
+
+            return kundene;
+            
+        }
+        
+    }
+}
+
---
+++ Models/Kunde.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace KundeApp.Models
+{
+    public class Kunde
+    {
+        public int id { get; set; }
+        public string navn { get; set; }
+        public string adresse { get; set; }
+    }
+}
+
